b6c7d573e24c93318847a25a3a72ced865f604a5
ilbuonmarcio <marciozgaming@gmail.com>
2023-01-30 23:46:13 +0100

    Added operating system

--- scripts/general.sh
+++ scripts/general.sh
@@ -3,7 +3,8 @@
 pacman -Sy &> /dev/null
 
 echo "Host: $(uname -n)\n"
-echo "Uptime:\n\n$(uptime --pretty)\n(since $(uptime --since))\n"
+echo "Operating System: $(cat /etc/*-release | grep "^NAME=" | sed -e 's/NAME=//g' | sed -e 's/\"//g')\n"
+echo "Uptime: $(uptime --pretty)\n(since $(uptime --since))\n"
 echo "Load averages: $(uptime | grep -ohe 'load average[s:][: ].*' | awk '{ print $3, $4, $5 }')\n"
 echo "Memory status (Installed/Available/Free): $(lsmem | grep "Total online memory" | awk '{print $4}' | sed -e 's/G//g').0G / $(free -h -w | head -n 2 | tail -n 1 | awk '{print $2" / "$8}' | sed -e 's/i//g')\n"
 echo "Available addresses:\n\n$(ip addr | grep 'inet ' | grep -v 'host lo' | grep -v 'br-' | grep -v 'docker0' | awk '{print "IP: "$2"\tBroadcast: "$4}')\n"
