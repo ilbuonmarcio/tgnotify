5140cc60b3ccff127f100cbb1beef0255f31d938
ilbuonmarcio <marciozgaming@gmail.com>
2023-09-24 18:25:55 +0200

    Fixed threshold on short arch

--- scripts/short_arch.sh
+++ scripts/short_arch.sh
@@ -5,7 +5,7 @@ pacman -Sy &> /dev/null
 echo "$(uname -n): $(uptime --pretty)"
 echo "Updates available ($(pacman -Qu | wc -l)):\n\n$(pacman -Qu)"
 
-ALERT=5
+ALERT=90
 df -H | grep -vE '^Filesystem|tmpfs|cdrom|overlay' | awk '{ print $5 " " $1 }' | while read -r output;
 do
   usep=$(echo "$output" | awk '{ print $1}' | cut -d'%' -f1 )
