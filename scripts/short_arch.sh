6aedefae1be8ab6c37d2eda55a7eff5a9f240111
ilbuonmarcio <marciozgaming@gmail.com>
2023-09-24 16:13:30 +0000

    Fixed overlay

--- scripts/short_arch.sh
+++ scripts/short_arch.sh
@@ -6,7 +6,7 @@ echo "$(uname -n): $(uptime --pretty)"
 echo "Updates available ($(pacman -Qu | wc -l)):\n\n$(pacman -Qu)"
 
 ALERT=5
-df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read -r output;
+df -H | grep -vE '^Filesystem|tmpfs|cdrom|overlay' | awk '{ print $5 " " $1 }' | while read -r output;
 do
   usep=$(echo "$output" | awk '{ print $1}' | cut -d'%' -f1 )
   partition=$(echo "$output" | awk '{ print $2 }' )
--- scripts/short_ubuntu.sh
+++ scripts/short_ubuntu.sh
@@ -5,8 +5,8 @@ apt update &> /dev/null
 echo "$(uname -n): $(uptime --pretty)"
 echo "Updates available: $(apt list --upgradable 2> /dev/null | sed 1,1d | wc -l)"
 
-ALERT=5
-df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read -r output;
+ALERT=90
+df -H | grep -vE '^Filesystem|tmpfs|cdrom|overlay' | awk '{ print $5 " " $1 }' | while read -r output;
 do
   usep=$(echo "$output" | awk '{ print $1}' | cut -d'%' -f1 )
   partition=$(echo "$output" | awk '{ print $2 }' )
